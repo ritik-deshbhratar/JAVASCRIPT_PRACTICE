ef3d2af30b2217d694e937b8a4a7502629fefff2
ritik <rdeshabhratar@gmail.com>
2024-07-15 15:24:20 +0530

    first Project using javaScript,html,css Calculator which perform all the oiperation like + - * % / ^ and shows the output on input box

---
+++ Project1ArithmaticCalc/CalcJS/index.js
@@ -0,0 +1,22 @@
+function Sum() {
+  let sum = (OutPut.value = parseInt(firstNum.value) + parseInt(SectNum.value));
+  //document.write("your sum is " + sum);
+}
+function Minus() {
+  let Minus = (OutPut.value = firstNum.value - SectNum.value);
+}
+
+function Multiply() {
+  let Multiply = (OutPut.value = firstNum.value * SectNum.value);
+}
+
+function Divide() {
+  let Divide = (OutPut.value = firstNum.value / SectNum.value);
+}
+
+function Modulo() {
+  let Modulo = (OutPut.value = firstNum.value % SectNum.value);
+}
+function Power() {
+  let Power = (OutPut.value = firstNum.value ** SectNum.value);
+}
